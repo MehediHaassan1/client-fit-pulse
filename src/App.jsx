69fce872a25b5aaf0bcb8978e473aacaaaf3ee29
Mehedi Hassan Barat <mehedi.haassan1@gmail.com>
2024-02-06 07:43:46 +0600

    basic setup done with color, font, tailwind, router

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline bg-bkg text-txt">
+                Hello world!
+            </h1>
+        </>
+    );
+}
+
+export default App;
